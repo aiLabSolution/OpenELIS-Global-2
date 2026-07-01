6fd4b942ab6dabc02fa14d7b886e7b8f2dca01df
Marloe Uy <marloe.uy.jr@gmail.com>
2026-07-01 15:56:46 +0800

    LIS-95: reject SD1 calibration imports

--- src/test/java/org/openelisglobal/analyzerimport/action/AnalyzerFhirImportControllerTest.java
+++ src/test/java/org/openelisglobal/analyzerimport/action/AnalyzerFhirImportControllerTest.java
@@ -82,6 +82,27 @@ public class AnalyzerFhirImportControllerTest extends BaseWebContextSensitiveTes
     }
 
     @Test
+    public void importFhirBundle_CalibrationDiagnosticReport_ReturnsBadRequestAndDoesNotInsert() throws Exception {
+        String bundleJson = "{"
+                + "\"resourceType\":\"Bundle\",\"type\":\"transaction\",\"entry\":["
+                + "{\"fullUrl\":\"urn:uuid:specimen-1\",\"resource\":{"
+                + "\"resourceType\":\"Specimen\",\"identifier\":[{\"value\":\"SD1-CAL-001\"}]"
+                + "}},"
+                + "{\"resource\":{\"resourceType\":\"DiagnosticReport\","
+                + "\"meta\":{\"tag\":[{\"system\":\"http://openelis-global.org/fhir/tags\",\"code\":\"CALIBRATION\"}]},"
+                + "\"status\":\"preliminary\",\"code\":{\"text\":\"Analyzer Results\"},"
+                + "\"specimen\":[{\"reference\":\"urn:uuid:specimen-1\"}]"
+                + "}}"
+                + "]}";
+
+        mockMvc.perform(post("/analyzer/fhir").contentType(MediaType.APPLICATION_JSON).content(bundleJson))
+                .andExpect(status().isBadRequest()).andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.errorKey").value("analyzer.fhirImport.error.calibrationRejected"));
+
+        verify(analyzerResultsService, never()).insertAnalyzerResults(anyList(), eq("1"));
+    }
+
+    @Test
     public void importFhirBundle_ValidObservation_InsertsAnalyzerResults() throws Exception {
         String bundleJson = "{\n" + "  \"resourceType\": \"Bundle\",\n" + "  \"type\": \"transaction\",\n"
                 + "  \"entry\": [\n" + "    {\n" + "      \"fullUrl\": \"urn:uuid:specimen-1\",\n"
